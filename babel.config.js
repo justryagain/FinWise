a9e5e571ee973b97b7664320263b9ae68e09a565
jstryagain <andrei_28_1997@hotmail.com>
2025-02-27 01:09:30 +0000

    @env setup and supabase connection config

---
+++ babel.config.js
@@ -0,0 +1,41 @@
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: [
+            '.ios.ts',
+            '.android.ts',
+            '.ts',
+            '.js',
+            '.jsx',
+            '.ios.tsx',
+            '.android.tsx',
+            '.tsx',
+            '.jsx',
+            '.js',
+            '.json',
+          ],
+          alias: {
+            '@app': './src',
+          },
+        },
+      ],
+    ],
+  };
+};
