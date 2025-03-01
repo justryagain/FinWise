d3829003f5a98b52654230dcd1e747f035f2670a
jstryagain <andrei_28_1997@hotmail.com>
2025-03-01 01:44:57 +0000

    Login page, fornt end

--- App.tsx
+++ App.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './Login.tsx'; // adjust the path as needed
+import SignupScreen from './Signup.tsx';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: false}}>
         <Stack.Screen name="Login" component={LoginScreen} />
-        {/* Add more screens here */}
+        <Stack.Screen name="SignUp" component={SignupScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
